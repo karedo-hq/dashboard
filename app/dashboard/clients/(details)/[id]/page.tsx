12a00956b7cf31d09a050ce1a327794382f20c85
ClementeSerrano <clementeserranosutil@gmail.com>
2024-03-05 10:36:46 +0100

    :sparkles: Add tabs component and integrate with client details ui

--- app/dashboard/clients/(details)/[id]/page.tsx
+++ app/dashboard/clients/(details)/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ClientDetailsHeader from '../../components/clients-details-header';
 import { findClientAction } from '../../lib/actions/find-client';
 
@@ -21,6 +22,20 @@ export default async function ClientDetailsPage(props: ClientDetailsPageProps) {
   return (
     <section className="flex flex-col">
       <ClientDetailsHeader client={client} />
+      <Tabs defaultValue="profile" className="p-8 pt-0">
+        <TabsList className="w-full">
+          <TabsTrigger value="profile">Persönlich</TabsTrigger>
+          <TabsTrigger value="guardianship">Betreuung</TabsTrigger>
+          <TabsTrigger value="health">Gesundheit</TabsTrigger>
+          <TabsTrigger value="residence">Aufenthalt</TabsTrigger>
+          <TabsTrigger value="wealth">Vermögen</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">Make changes to your account here.</TabsContent>
+        <TabsContent value="guardianship">Make changes to your account here.</TabsContent>
+        <TabsContent value="health">Make changes to your account here.</TabsContent>
+        <TabsContent value="residence">Make changes to your account here.</TabsContent>
+        <TabsContent value="wealth">Make changes to your account here.</TabsContent>
+      </Tabs>
     </section>
   );
 }
